e97448804f3986606fab6e9ab0b7419af3e35b23
jomichaelis <johannesmichaelis0@gmail.com>
2022-10-11 01:03:53 +0200

    added dist-public

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr: false,
     css: [
         'vuetify/lib/styles/main.sass',
         'mdi/css/materialdesignicons.min.css',
-    ],
-    modules: [
-        '@nuxt/image-edge',
     ],
     build: {
         transpile: ['vuetify'],
