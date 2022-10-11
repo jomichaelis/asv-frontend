84773c2b243ad454ae53e88daf537ebce225689a
jomichaelis <johannesmichaelis0@gmail.com>
2022-10-11 09:36:57 +0200

    testing axios base64 images

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,29 @@
 <template>
   <v-container>
     <h2>Dashboard</h2>
+    <img :src="`data:image/png;base64,${this.imageData}`" alt="test" />
   </v-container>
 </template>
+
+<script>
+
+export default {
+  async fetch() {
+    const vm = this
+    this.$axios
+      .get("https://asv-api.jomichaelis.de/api/v21/preview-image/")
+      .then(function(response) {
+        console.log(response)
+        console.log(new Buffer(response.data, 'binary').toString('base64'))
+        console.log(vm)
+        vm.imageData = new Buffer(response.data, 'binary').toString('base64')
+      })
+    // .then(response => console.log(response))
+  },
+  data() {
+    return {
+      imageData: 'abc'
+    }
+  },
+}
+</script>
